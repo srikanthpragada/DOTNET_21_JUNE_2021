ce9cf29bb9049d9f57d3025fd889e4b3fb573654
srikanthpragada <srikanthpragada@gmail.com>
2021-06-23 17:59:20 +0530

    23-JUNE

---
+++ csharpdemo/Factors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class Factors
+    {
+        static void Main(string[] args)
+        {
+            // Take number from user
+            Console.Write("Enter a number :");
+            int num = Int32.Parse(Console.ReadLine());
+
+            for(int i = 2; i <= num/2; i ++)
+            {
+                if (num % i == 0)
+                    Console.WriteLine($"{i} is a factor for {num}"); // String interpolation 
+            }
+
+        }
+    }
+}
---
+++ csharpdemo/PrintNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class PrintNumbers
+    {   
+        static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
