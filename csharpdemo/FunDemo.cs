65b6c66fcb0020316c2b7fa6843813434d0d5b93
srikanthpragada <srikanthpragada@gmail.com>
2021-06-24 17:55:01 +0530

    24-JUNE

---
+++ csharpdemo/FunDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class FunDemo
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Wish(String name = "Guest", String message = "Hello")
+        {
+            Console.WriteLine($"{message} {name}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(10, 20));
+            Wish("Scott", "Hi");
+            Wish("Steve");
+            Wish();
+            Wish(message : "Hi");  // Pass value by name 
+        }
+    }
+}
