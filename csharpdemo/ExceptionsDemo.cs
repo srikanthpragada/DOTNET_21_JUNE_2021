72156fa8cc3ac7e4075fd748f7cf56593307a3fc
srikanthpragada <srikanthpragada@gmail.com>
2021-06-30 17:57:53 +0530

    30-JUNE

---
+++ csharpdemo/ExceptionsDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class ExceptionsDemo
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b;
+            try
+            {
+                b = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(a / b);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Invalid operations. Division by zero!");
+            }
+            finally
+            {
+                Console.WriteLine("Finally!");
+            }
+          
+            Console.WriteLine("The End");
+        }
+    }
+}
