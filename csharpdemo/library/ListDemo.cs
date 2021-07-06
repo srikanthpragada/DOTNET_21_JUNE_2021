7c6a954d33593761647206549c08ff930e61741b
srikanthpragada <srikanthpragada@gmail.com>
2021-07-06 18:57:02 +0530

    06-JULY

---
+++ csharpdemo/library/ListDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo.library
+{
+    class ListDemo
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<String>();
+
+            names.Add("Bill");
+            names.Add("Larry");
+            names.Add("Mark");
+            names.Insert(0, "Steve");
+            names.Remove("Bill");
+            names.RemoveAt(0);
+ 
+            foreach(var n in names)
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ csharpdemo/library/SortNames.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo.library
+{
+    class SortNames
+    {
+        static void Main(string[] args)
+        {
+            var file = File.OpenText(@"c:\classroom\jun21\names.txt");
+            var names = new SortedSet<String>();
+
+            while (true)
+            {
+                var line = file.ReadLine();
+                if (line == null)
+                    break;
+
+                var linenames = line.Split(',');
+                foreach (var n in linenames)
+                    names.Add(n.Trim());
+            }
+
+            file.Close();
+
+            foreach(var n in names)
+                Console.WriteLine(n);
+        }
+    }
+}
