5e5f5438db5a456de35af33a230549429066579b
srikanthpragada <srikanthpragada@gmail.com>
2021-07-19 17:56:50 +0530

    19-JULY

---
+++ webdemo/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webdemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+        public string Now { get; set; }
+        public void OnGet()   // Executed when a request is made 
+        {
+            Message = "Welcome to Asp.Net Core";
+            Now = DateTime.Now.ToString();
+        }
+    }
+}
