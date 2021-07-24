63aae5cd9ca1a8d8b736c05fdbfd022bef7ea731
srikanthpragada <srikanthpragada@gmail.com>
2021-07-24 17:57:09 +0530

    24-JULY

---
+++ webdemo/Pages/UploadPhoto.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webdemo.Pages
+{
+
+    public class UploadPhotoModel : PageModel
+    {
+        public IFormFile Photo { get; set; }  // Contains file uploaded 
+        public IWebHostEnvironment Env { get; }
+
+        public UploadPhotoModel(IWebHostEnvironment Env)  // DI (IOC)
+        {
+            Console.WriteLine(Env.GetType().Name);
+            this.Env = Env;
+        }
+        public void OnPost()
+        {
+            var filePath = Env.WebRootPath + "/photos/" + Photo.FileName;
+            Console.WriteLine(filePath);
+
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                Photo.CopyTo(stream); // Make a copy of uploaded file 
+            }
+        }
+    }
+}
