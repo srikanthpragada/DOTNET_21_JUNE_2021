46f81b306f751e73a9a50483dfa9c6f9a3688ff8
srikanthpragada <srikanthpragada@gmail.com>
2021-08-05 17:58:55 +0530

    05-AUG

--- Books/Pages/List.cshtml.cs
+++ Books/Pages/List.cshtml.cs
@@ -5,21 +5,22 @@ using System.Threading.Tasks;
 using Books.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+ 
 namespace Books.Pages
 {
     public class ListModel : PageModel
     {
-        public List<Book> Books { get; set; }
+        public List<Book> Books { get; set; } 
         public CatalogContext Context { get; set; }
         public string Message { get; set; }
         public ListModel(CatalogContext ctx)
-        {
+        {  
             Context = ctx;
         }
         public void OnGet()
         {
-            Books = Context.Books.ToList<Book>();
+            Books = Context.Books.ToList<Book>(); 
+             
         }
     }
 }
--- Books/Pages/Search.cshtml.cs
+++ Books/Pages/Search.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Books.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,16 +12,37 @@ namespace Books.Pages
     public class SearchModel : PageModel
     {
         [BindProperty]
-        [Required]
         public string Title { get; set; }
 
-        [Required]
         [BindProperty]
         public string Author { get; set; }
+
+        public List<Book> Books { get; set; }
+        public CatalogContext Context { get; set; }
+        public string Message { get; set; }
+        public SearchModel(CatalogContext ctx)
+        {
+            Context = ctx;
+        }
 
         public void OnGet()
         {
         }
- 
+
+        public void OnPost()
+        {
+            if (Title is null)
+                Title = "";
+
+            if (Author is null)
+                Author = "";
+
+            Books = Context.Books
+                  .Where(b => b.Title.Contains(Title) && b.Author.Contains(Author))
+                  .ToList<Book>();
+
+           
+        }
+
     }
 }
